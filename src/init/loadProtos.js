1a77394022943d7e74819fee0dc15de2aebe5b56
ohyudan <ohyudan86@gmail.com>
2024-11-13 19:32:09 +0900

    add: yarn install...etc

---
+++ src/init/loadProtos.js
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import protobuf from 'protobufjs';
+import packetNames from '../protobuf/packetsName.js';
+import CustomError from '../utils/error/customError.js';
+import { ErrorCodes } from '../utils/error/errorCodes.js';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const protoFolder = path.join(dirname, '../protobuf');
+
+/**
+ * .proto 파일들을 전부 가져오는 함수
+ * @param {*} protoFolder
+ * @param {*} fileList
+ * @returns
+ */
+const getAllProtoFiles = (protoFolder, fileList = []) => {
+  const files = fs.readdirSync(protoFolder);
+
+  files.forEach((file) => {
+    const filePath = path.join(protoFolder, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      getAllProtoFiles(filePath, fileList);
+    } else if (path.extname(file) === '.proto') {
+      fileList.push(filePath);
+    }
+  });
+  return fileList;
+};
+
+const protoFiles = getAllProtoFiles(protoFolder);
+
+const protoMessages = {};
+
+/**
+ * protoMessages에 가져온 protoFiles를 등록하는 함수
+ */
+export const loadProtos = async () => {
+  try {
+    const root = new protobuf.Root();
+
+    await Promise.all(
+      protoFiles.map((file) => {
+        return root.load(file);
+      }),
+    );
+
+    for (let [packageName, types] of Object.entries(packetNames)) {
+      protoMessages[packageName] = {};
+
+      for (const [protoType, typeName] of Object.entries(types)) {
+        try {
+          protoMessages[packageName][protoType] = root.lookupType(typeName);
+        } catch (error) {
+          protoMessages[packageName][protoType] = root.lookupEnum(typeName);
+        }
+      }
+    }
+
+    console.log(`프로토 타입 로드에 끝났습니다.`);
+  } catch (error) {
+    throw new CustomError(ErrorCodes.PROTOFILE_LOADING_FAIL, `프로토 로딩 중 에러 발생`);
+  }
+};
+
+/**
+ * 등록한 protoMessages를 가져오는 함수
+ * @returns {Object}
+ */
+export const getProtoMessages = () => {
+  return { ...protoMessages };
+};
