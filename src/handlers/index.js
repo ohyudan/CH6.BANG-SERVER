270f0fc001656134f37bcc3c094b7f1f8cfa49e0
ohyudan <ohyudan86@gmail.com>
2024-11-14 20:54:35 +0900

    fix: createResponse.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,52 +2,57 @@ import HANDLER_IDS from '../constants/handlerIds.js';
 import registerHandler from './auth/register.handler.js';
 const packetTypes = {
   [HANDLER_IDS.REGISTER_REQUEST]: {
-    packetType: undefined,
-    protoType: 'GamePacket',
+    packetType: registerHandler,
     protoType: 'C2SRegisterRequest',
   },
 };
-
+/**
+ * 패킷타입에 맞는 핸들러로 분배해주는 함수
+ * @param {Socket} socket
+ * @param {Number} packetType
+ * @param {Object} payload
+ */
 export const handler = async (socket, packetType, payload) => {
   try {
     const handlerFunction = packetTypes[packetType].packetType;
     if (!handlerFunction) {
-      //throw new CustomError();
+      throw new CustomError(ErrorCodes.UNKNOWN_HANDLER_ID, `핸들러를 찾을 수 없습니다`);
     }
     await handlerFunction({ socket, payload });
   } catch (err) {
     //await handlerError(socket, err);
   }
 };
-export const getHandlerById = (packetType) => {
-  if (!handlers[packetType]) {
-    throw new CustomError(
-      ErrorCodes.UNKNOWN_HANDLER_ID,
-      `[${packetType}] HandlerID의 핸들러를 찾을 수 없습니다.`,
-    );
-  }
 
-  return handlers[packetType].handler;
-};
+// export const getHandlerById = (packetType) => {
+//   if (!handlers[packetType]) {
+//     throw new CustomError(
+//       ErrorCodes.UNKNOWN_HANDLER_ID,
+//       `[${packetType}] HandlerID의 핸들러를 찾을 수 없습니다.`,
+//     );
+//   }
 
-export const getProtoTypeById = (packetType) => {
-  if (!handlers[packetType]) {
-    throw new CustomError(
-      ErrorCodes.UNKNOWN_HANDLER_ID,
-      `[${packetType}] HandlerID의 프로토타입을 찾을 수 없습니다.`,
-    );
-  }
+//   return handlers[packetType].handler;
+// };
 
-  return handlers[packetType].protoType;
-};
+// export const getProtoTypeById = (packetType) => {
+//   if (!handlers[packetType]) {
+//     throw new CustomError(
+//       ErrorCodes.UNKNOWN_HANDLER_ID,
+//       `[${packetType}] HandlerID의 프로토타입을 찾을 수 없습니다.`,
+//     );
+//   }
 
-export const getProtoPayloadTypeById = (packetType) => {
-  if (!handlers[packetType]) {
-    throw new CustomError(
-      ErrorCodes.UNKNOWN_HANDLER_ID,
-      `[${packetType}] HandlerID의 프로토타입 페이로드를 찾을 수 없습니다.`,
-    );
-  }
+//   return handlers[packetType].protoType;
+// };
 
-  return handlers[packetType].protoPayloadType;
-};
+// export const getProtoPayloadTypeById = (packetType) => {
+//   if (!handlers[packetType]) {
+//     throw new CustomError(
+//       ErrorCodes.UNKNOWN_HANDLER_ID,
+//       `[${packetType}] HandlerID의 프로토타입 페이로드를 찾을 수 없습니다.`,
+//     );
+//   }
+
+//   return handlers[packetType].protoPayloadType;
+// };
