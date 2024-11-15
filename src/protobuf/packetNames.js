fd6bde7bc733bcaa41bf20aac20ac9244bfe0109
ohyudan <ohyudan86@gmail.com>
2024-11-15 17:37:46 +0900

    fix: patcketNames.js

--- src/protobuf/packetNames.js
+++ src/protobuf/packetNames.js
@@ -5,6 +5,19 @@ const packetNames = {
   failCode: {
     GlobalFailCode: 'failCode.GlobalFailCode',
   },
+  room: {
+    RoomData: 'room.RoomData',
+    RoomStateType: 'room.RoomStateType',
+  },
+  user: {
+    UserData: 'user.UserData',
+    CharacherData: 'user.CharacterData',
+    CharacterType: 'user.CharacterType',
+    CharacterPositionData: 'user.CharacterPositionData',
+    CharacterStateType: 'user.CharacterStateType',
+    RoleType: 'user.RoleType',
+  },
+  card: {},
 };
 
 export default packetNames;
