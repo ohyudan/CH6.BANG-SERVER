588abe0ce0531a88355af74a214dc2267cda1f92
ohyudan <ohyudan86@gmail.com>
2024-11-19 10:32:50 +0900

    chore: 주석 제거 및 처리

--- src/handlers/auth/login.handler.js
+++ src/handlers/auth/login.handler.js
@@ -1,4 +1,4 @@
-import { createResponse, failCodeReturn } from '../../utils/response/createResponse.js';
+import { createResponse } from '../../utils/response/createResponse.js';
 import { handler } from '../index.js';
 import { handlerError } from '../../error/errorHandler.js';
 import Config from '../../config/config.js';
@@ -6,33 +6,33 @@ import HANDLER_IDS from '../../constants/handlerIds.js';
 import { createUser, findUserById } from '../../dataBase/user/user.db.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'; //jwt토큰 발급을 위한 jwt 임포트
+import createFailCode from '../../utils/response/createFailCode.js';
 
 //email을 어떻게 할것인지 생각해봐야함
 const loginHandler = async ({ socket, payload }) => {
   const { email, password } = payload;
-  let failCode = failCodeReturn(0);
+  let failCode = createFailCode(0);
   let success = true;
   let message = 'login success';
   try {
     console.log(email, password);
     const emailExists = await findUserById(email);
 
-    if (emailExists === null)//id 중복을 검사하는 if문
-    {
-      failCode = 3;//LOGIN_FAIL
+    if (emailExists === null) {
+      //id 중복을 검사하는 if문
+      failCode = 3; //LOGIN_FAIL
       message = 'ID is not exists';
       success = false;
-      console.error("ID is not exists");
+      console.error('ID is not exists');
     }
-    if (!(await bcrypt.compare(password, emailExists.password)))//id로찾아낸 db의 정보와 비밀번호 대조
-    {
-      failCode = 3;//LOGIN_FAIL
+    if (!(await bcrypt.compare(password, emailExists.password))) {
+      //id로찾아낸 db의 정보와 비밀번호 대조
+      failCode = 3; //LOGIN_FAIL
       message = 'Password is dismatch.';
       success = false;
-      console.error("Password is dismatch.");
+      console.error('Password is dismatch.');
     }
     const jwtToken = jwt.sign({ email, password }, 'SECRET_KEY', { expiresIn: '1h' }); //SECRET_KEY부분임시로 채움, 만료시간 1시간으로 설정
-
 
     const S2CLoginResponse = {
       success,
--- src/handlers/index.js
+++ src/handlers/index.js
@@ -4,8 +4,8 @@ import { CustomError } from '../error/customError.js';
 import { ErrorCodes } from '../error/errorCodes.js';
 import loginHandler from './auth/login.handler.js';
 import registerHandler from './auth/register.handler.js';
-import testlogin from './auth/test-login.js';
 import roomListGetHandler from './room/roomListGetHandler.js';
+
 const packetTypes = {
   [HANDLER_IDS.REGISTER_REQUEST]: {
     packetType: registerHandler,
@@ -41,36 +41,3 @@ export const handler = async (socket, packetType, payload) => {
     //await handlerError(socket, err);
   }
 };
-
-// export const getHandlerById = (packetType) => {
-//   if (!handlers[packetType]) {
-//     throw new CustomError(
-//       ErrorCodes.UNKNOWN_HANDLER_ID,
-//       `[${packetType}] HandlerID의 핸들러를 찾을 수 없습니다.`,
-//     );
-//   }
-
-//   return handlers[packetType].handler;
-// };
-
-// export const getProtoTypeById = (packetType) => {
-//   if (!handlers[packetType]) {
-//     throw new CustomError(
-//       ErrorCodes.UNKNOWN_HANDLER_ID,
-//       `[${packetType}] HandlerID의 프로토타입을 찾을 수 없습니다.`,
-//     );
-//   }
-
-//   return handlers[packetType].protoType;
-// };
-
-// export const getProtoPayloadTypeById = (packetType) => {
-//   if (!handlers[packetType]) {
-//     throw new CustomError(
-//       ErrorCodes.UNKNOWN_HANDLER_ID,
-//       `[${packetType}] HandlerID의 프로토타입 페이로드를 찾을 수 없습니다.`,
-//     );
-//   }
-
-//   return handlers[packetType].protoPayloadType;
-// };
