6b45dc1ad68df13f54dd1dd76ff7b82f875fd3c9
ohyudan <ohyudan86@gmail.com>
2024-11-19 16:08:06 +0900

    fix: room.class

--- src/utils/room/room.class.js
+++ src/utils/room/room.class.js
@@ -6,7 +6,7 @@ import { message } from 'protocol-buffers/compile.js';
  */
 class Room {
   constructor(id, ownerId, name, maxUserNum) {
-    this._id = id;
+    this._id = id; // 방 아이디
     this._ownerId = ownerId;
     this._name = name;
     this._maxUserNum = maxUserNum;
@@ -18,7 +18,11 @@ class Room {
 
     //this.addplayer(ownerId);
   }
-  getRoomData() {}
+
+  getRoomData() {
+    // RoomData 형식에 맞게 전송 해야됨
+    //return
+  }
 
   setState(state) {
     const result = this._state.setState(state);
@@ -28,17 +32,33 @@ class Room {
     return this._state.getCurrentStateData();
   }
 
+  /**
+   *
+   * @param {Player}
+   * @returns {bool} 성공 시 true  실패 시 false
+   */
   addplayer(player) {
     const currentUserNumber = this._playerlist.size();
     if (this._maxUserNum <= currentUserNumber) {
       return false;
     }
-    this._playerlist.set(currentUserNumber, this._id);
+    this._playerlist.set(player.id, player);
     return true;
   }
+  /**
+   *
+   * @param {Player}
+   * @returns {bool} 성공 시 true  실패 시 false
+   */
+  subplayer(player) {
+    const currentUserNumber = this._playerlist.size();
+    // 소유자가 나가면 어떻게 처리가 되는지
+    // 그 외에는 id로 제거
+  }
+  setState(Number) {
+    this._state.getCurrentStateData(Number);
+  }
 
-  subplayer(player) {}
-  setState(Number) {}
   getAllPlayers() {
     return this.playerlist;
   }
