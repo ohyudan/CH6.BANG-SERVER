78ec660aef86bd430f1dd85675ab174b843a0d70
hjy2007 <wjddud2007@gmail.com>
2024-11-22 11:31:54 +0900

    rename useCardHandler.js

--- src/handlers/cards/useCardHandler.js
+++ src/handlers/cards/useCardHandler.js
@@ -7,32 +7,6 @@ export const useCardHandler = ({ socket, payload }) => {
   const { cardType, targetUserId } = payload;
   let failCode = createFailCode(0);
 
-  //   CardType {
-  //     NONE = 0;
-  //     BBANG = 1; // 20장
-  //     BIG_BBANG = 2; // 1장
-  //     SHIELD = 3; // 10장
-  //     VACCINE = 4; // 6장
-  //     CALL_119 = 5; // 2장
-  //     DEATH_MATCH = 6; // 4장
-  //     GUERRILLA = 7; // 1장
-  //     ABSORB = 8; // 4장
-  //     HALLUCINATION = 9; // 4장
-  //     FLEA_MARKET = 10; // 3장
-  //     MATURED_SAVINGS = 11; // 2장
-  //     WIN_LOTTERY = 12; // 1장
-  //     SNIPER_GUN = 13; // 1장 //
-  //     HAND_GUN = 14; // 2장
-  //     DESERT_EAGLE = 15; // 3장
-  //     AUTO_RIFLE = 16; // 2장 //
-  //     LASER_POINTER = 17; // 1장
-  //     RADAR = 18; // 1장
-  //     AUTO_SHIELD = 19; // 2장
-  //     STEALTH_SUIT = 20; // 2장
-  //     CONTAINMENT_UNIT = 21; // 3장
-  //     SATELLITE_TARGET = 22; // 1장
-  //     BOMB = 23; // 1장
-  // }
   try {
     // cardType에 따라 다른 로직 적용
     switch (cardType) {
@@ -40,28 +14,94 @@ export const useCardHandler = ({ socket, payload }) => {
       // 2 (무차별난사)
       // 4 (백신)
       case 4: {
-        const player = getPlayer(socket.id)
-        player.removeHandCard(4)
-        player.increaseHp()
-        player.decreaseHandCardsCount()
+        const player = getPlayer(socket.id);
+        player.removeHandCard(4);
+        player.increaseHp();
+        player.decreaseHandCardsCount();
+        // 느낌만
 
         const S2CUseCardResponse = {
           succes: true,
           failCode: failCode,
         };
-
         const S2CUseCardNotification = {
           cardType: cardType,
           userId: socket.id,
           targetUserId: targetUserId,
         };
-
         const S2CCardEffectNotification = {
           cardType: cardType,
           userId: socket.id,
           succes: true,
         };
+        const S2CUserUpdateNotification = {
+          user: userData,
+        };
 
+        const responseGamePacket = { useCardResponse: S2CUseCardResponse };
+        const notificationGamePacket = { useCardNotification: S2CUseCardNotification };
+        const cardEffectNotificationGamePacket = {
+          cardEffectNotification: S2CCardEffectNotification,
+        };
+        const userUpdateGamePacket = { userUpdateNotification: S2CUserUpdateNotification };
+
+        const responseResult = createResponse(
+          HANDLER_IDS.USE_CARD_REQUEST,
+          socket.version,
+          socket.sequence,
+          responseGamePacket,
+        );
+        const notificationResult = createResponse(
+          HANDLER_IDS.USE_CARD_NOTIFICATION,
+          socket.version,
+          socket.sequence,
+          notificationGamePacket,
+        );
+        const effectResult = createResponse(
+          HANDLER_IDS.CARD_EFFECT_NOTIFICATION,
+          socket.version,
+          socket.sequence,
+          cardEffectNotificationGamePacket,
+        );
+        const userUpdateResult = createResponse(
+          HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+          socket.version,
+          socket.sequence,
+          userUpdateGamePacket,
+        );
+
+        socket.write(responseResult);
+        socket.write(notificationResult);
+        socket.write(effectResult);
+        socket.write(userUpdateResult);
+      }
+
+      // 5 (119호출)
+      case 5: {
+        const player = getPlayer(socket.id);
+        player.removeHandCard(5);        
+        player.decreaseHandCardsCount();
+        if (targetUserId !== 0) {
+        player.increaseHp();
+        } else {
+          // 플레이어를 제외한 모든 플레이어의 hp +1
+        }
+        // 느낌만
+
+        const S2CUseCardResponse = {
+          succes: true,
+          failCode: failCode,
+        };
+        const S2CUseCardNotification = {
+          cardType: cardType,
+          userId: socket.id,
+          targetUserId: targetUserId,
+        };
+        const S2CCardEffectNotification = {
+          cardType: cardType,
+          userId: socket.id,
+          succes: true,
+        };
         const S2CUserUpdateNotification = {
           user: userData,
         };
@@ -104,7 +144,6 @@ export const useCardHandler = ({ socket, payload }) => {
         socket.write(userUpdateResult);
       }
     }
-    // 5 (119호출)
     // 6 (현피)
     // 7 (게릴라전)
     // 11 (만기적금)
