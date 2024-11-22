4281f0cc148f67f897f957a48328dff7528dcf72
ohyudan <ohyudan86@gmail.com>
2024-11-22 17:48:44 +0900

    fix : json 셋팅

--- src/init/initServer.js
+++ src/init/initServer.js
@@ -1,10 +1,11 @@
 import { loadProtos } from './loadProtos.js';
-
+import { loadGameAssets } from './loadGameAssets.js';
 /**
  * 서버 시작시 필요한 정보를 로드하는 함수
  */
 const initServer = async () => {
   await loadProtos();
+  await loadGameAssets();
 };
 
 export default initServer;
