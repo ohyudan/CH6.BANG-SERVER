2c88a3ea9fe572769924f67ef3e3df6d8239a6f2
ohyudan <ohyudan86@gmail.com>
2024-11-26 15:48:05 +0900

    chore: 주석 처리

--- src/model/room/roomList.class.js
+++ src/model/room/roomList.class.js
@@ -15,7 +15,7 @@ class RoomList extends Observer {
    */
   getRoomList() {
     // 요청 받을 떄마다 하지말고 방 생성 / 방 삭제 / 인원 변경 / 상태 변경 / 에만 새로고침할 수 있지않을까.
-    // -> 옵저버 패턴의 추가로 Update를 만들면 가능해졌음
+    // -> 옵저버 패턴의 추가로 갱신 받을 때마다 보내주기 가능
 
     const roomStateList = this.getWaitStateRoom();
     const rooms = [];
