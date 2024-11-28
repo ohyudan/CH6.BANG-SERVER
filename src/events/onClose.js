f087e0163df9d50730d6161b8d4e9c5ee0842d03
이상민 <yihs97@naver.com>
2024-11-28 11:26:25 +0900

    fix : onClose.js 및 subPlayer 수정

--- src/events/onClose.js
+++ src/events/onClose.js
@@ -12,23 +12,33 @@ const onClose = (socket) => async () => {
 
     // 플레이어가 속한 방 처리
     const roomId = player.currentRoomId;
-    if (roomId !== undefined && roomId !== null) {
+
+    if (roomId === null) {
+      console.log(`플레이어 ${player.id}는 방에 소속되지 않은 상태입니다.`);
+      playerList.subPlayer(player.id); // 플레이어 리스트에서 제거
+      console.log(`플레이어 리스트에서 플레이어 ${player.id} 제거 완료.`);
+      return;
+    } else if (roomId !== null) {
       const room = roomList.getRoom(roomId);
 
       if (room) {
-        console.log(`${room.id}번 방에서, 플레이어 ID: ${player.id} 제거됨`);
+        // 방에서 플레이어 제거
         room.subPlayer(player);
+        console.log(`${room.id}번 방에서, 플레이어 ID: ${player.id} 제거됨`);
 
         // 방에 플레이어가 없으면 방 삭제
         if (room.getAllPlayers().size === 0) {
           console.log(`${room.id}번 방 삭제`);
           roomList.subRoomList(room);
+        } else {
+          console.log(`${room.id}번 방이 유지됨. 방장: ${room.ownerId}`);
         }
       } else {
         console.warn(`플레이어가 속한 방을 찾을 수 없습니다. Room ID: ${roomId}`);
       }
     }
     // 3. 플레이어 리스트에서 제거
+    console.log(`플레이어 ${player.id}을 플레이어 리스트에서 제거.`);
     playerList.subPlayer(player.id);
   } catch (error) {
     console.error(`onClose 처리 중 오류 발생 ${error.message}`);
