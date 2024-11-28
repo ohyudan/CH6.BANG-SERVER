2bf7092c7f3d16912bc4fb62c7d0743ff2ad5e13
hjy2007 <wjddud2007@gmail.com>
2024-11-28 14:03:02 +0900

    fix game.class.js
    낮 시작시 카드 숫자 조정

--- src/model/game/game.class.js
+++ src/model/game/game.class.js
@@ -3,6 +3,7 @@ import { RoomStateType, STATE } from '../room/room.status.js';
 import phaseUpdateNotification from '../../utils/notification/phaseUpdate.notification.js';
 import { getGameAssets } from '../../init/loadGameAssets.js';
 import userUpdateNotification from '../../utils/notification/userDataUpdate.notification.js';
+import roomList from '../room/roomList.class.js';
 
 class Game {
   constructor(id, roomId, users) {
@@ -40,12 +41,15 @@ class Game {
         return false; // 유효하지 않은 Phase
     }
 
+    const room = roomList.getRoom(roomId);
+    const playerList = room.getAllPlayers();
+
     let changedPositions = new Map();
     if (nextPhase === PHASE.END) {
       // 현재 플레이어 캐릭터의 위치
       // users로 플레이어 class객체의 함수를 쓸 수 있는지 판단
-      for (i = 0; i < this.users.length; i++) {
-        let position = { id: this.users[i].id, x: this.users[i].getX(), y: this.users[i].getY() };
+      for (i = 0; i < playerList.length; i++) {
+        let position = { id: playerList[i].id, x: playerList[i].getX(), y: playerList[i].getY() };
         changedPositions.push(position);
       }
     } else {
@@ -55,19 +59,29 @@ class Game {
       const characterPositions = gameAssets.characterPositionData.position;
       // 위치 정보 셔플링 및 유저 위치 설정
       const selectedPositions = new Set();
-      while (selectedPositions.size < this.users.length) {
+      while (selectedPositions.size < playerList.length) {
         const randId = Math.floor(Math.random() * characterPositions.length);
         selectedPositions.add(characterPositions[randId]);
       }
 
       const posArr = [...selectedPositions];
-      for (i = 0; i < this.users.length; i++) {
-        let position = { id: this.users[i].id, x: posArr[i].x, y: posArr[y] };
+      for (i = 0; i < playerList.length; i++) {
+        let position = { id: playerList[i].id, x: posArr[i].x, y: posArr[y] };
         changedPositions.push(position);
       }
 
       // 낮 시작시 자신의 핸드가 자신의 체력 이상이면 랜덤으로 체력 수치만큼 카드를 버리도록 조정
-
+      for (i = 0; i < playerList.length; i++) {
+        if (playerList[i].handCardsCount > playerList[i].hp) {
+          while (playerList[i].handCards.length < playerList[i].hp) {
+            let DestroyCards = playerList[i].handCards.splice(
+              Math.floor(Math.random() * playerList[i].handCards.length),
+              1,
+            )[0];
+            console.log('id', playerList[i].id, '버려진 카드', DestroyCards);
+          }
+        }
+      }
 
       // 자신의 디버프를 체크 한 후 해당 디버프 적용
 
@@ -76,9 +90,8 @@ class Game {
       // 빵야 횟수 초기화
 
       // 캐릭터 정보 업데이트
-      userUpdateNotification()
+      userUpdateNotification();
     }
-
 
     // Phase 변경 및 다음 상태 전환 시간 설정
     // 이후 notification
