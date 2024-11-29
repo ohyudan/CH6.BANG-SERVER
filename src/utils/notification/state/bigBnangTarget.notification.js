42255b167eab5c13998a825ed67271bf4ae3771a
ohyudan <ohyudan86@gmail.com>
2024-11-29 20:40:53 +0900

    del: 삭제

--- src/utils/notification/state/bigBnangTarget.notification.js
+++
@@ -1,7 +0,0 @@
-import { createResponse } from '../../response/createResponse.js';
-import roomList from '../../../model/room/roomList.class.js';
-const bigBnangTargetNotification = (reactionType) => {
-  console.log(reactionType); // 0 false 1 true
-};
-
-export default bigBnangTargetNotification;
