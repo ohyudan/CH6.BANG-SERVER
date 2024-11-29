6f6ad36b8ca1c2156c76933cde726b98599122d4
hjy2007 <wjddud2007@gmail.com>
2024-11-29 21:02:58 +0900

    fix destroyCard.handler.js
    최종

--- src/handlers/inGame/destroyCard.handler.js
+++ src/handlers/inGame/destroyCard.handler.js
@@ -7,14 +7,13 @@ import { createResponse } from '../../utils/response/createResponse.js';
 const destroyCardHandler = async ({ socket, payload }) => {
   const { destroyCards } = payload; // 버릴 카드의 배열[{ cardType, count }, { cardType, count }]
 
-  const player = playerList.getPlayer(socket.id);
+  const player = playerList.getPlayer(socket.id);  
+  const room = roomList.getRoom(player.currentRoomId);
 
   destroyCards.forEach((value) => {
     player.removeHandCard(value.type);
     player.decreaseHandCardsCount();
   });
-
-  console.log(player.characterData.handCards);
 
   const S2CDestroyCardResponse = { handCards: player.characterData.handCards }; // 버린 이후 가지고 있는 카드
   const gamePacket = { destroyCardResponse: S2CDestroyCardResponse };
@@ -26,9 +25,24 @@ const destroyCardHandler = async ({ socket, payload }) => {
   );
 
   socket.write(response);
-  const roomId = player.currentRoomId();
-  const room = roomList.getRoom(roomId);
-  userUpdateNotification(room);
+
+  const inGameUsers = Array.from(room.getAllPlayers().values());
+
+  inGameUsers.forEach((player) => {
+    const S2CUserUpdateNotification = { user: player.getAllUsersData() };
+
+    const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
+
+    const userUpdateNotification = createResponse(
+      HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+      player.socket.version,
+      player.socket.sequence,
+      updatePacket,
+    );
+
+    player.socket.write(userUpdateNotification);
+  });
+
 };
 
 export default destroyCardHandler;
