ee4837185eebcb2db91820404a6a1dd6c78c0065
mingyu6688 <kmingyu6688@gmail.com>
2024-12-01 17:31:49 +0900

    fix: bbang

--- src/utils/card/bbang.js
+++ src/utils/card/bbang.js
@@ -13,117 +13,212 @@ const bbang = ({ socket, cardType, targetUserId }) => {
   const targetUser = playerList.getPlayer(targetUserId.low);
   const inGameUsers = Array.from(room.getAllPlayers().values());
 
-  // 유저의 무기별 발사 횟수 검증
-  // 유저가 레드or자동소총 발수 제약없음 / 아니면 핸드건은 2발 제한 나머진 1발
-  if (
-    !(
-      user.characterData.characterType === CHARACTER_TYPE.RED ||
-      user.characterData.weapon === CARD_TYPE.AUTO_RIFLE
-    )
-  ) {
-    // 핸드건인 경우 2발 제한
-    if (user.characterData.weapon === CARD_TYPE.HAND_GUN && user.characterData.bbangCount >= 2) {
-      console.log('발사 실패: 핸드건은 2발로 제한');
+  // 만약 내가 현피 상태라면? 다른 처리가 필요할듯
+  // if (user.characterData.stateInfo.state === CHARACTER_STATE_TYPE.DEATH_MATCH_TURN_STATE?.DEATH_MATCH_STATE) {}
 
-      // 사용자만 정보 업데이트 -> 클라이언트에서 사용한 카드가 사라지지 않게 업데이트 하는 것
-      const S2CUserUpdateNotification = { user: user.getAllUsersData() };
+  // 타겟 유저가 현재 NONE 상태일 때 발사 가능하게 처리 // 이거 클라에서 어떻게 처리되는지 확인필요 -> 클라에서 발사가 안되면 처리할 필요x
+  if (targetUser.characterData.stateInfo.state === CHARACTER_STATE_TYPE.NONE_CHARACTER_STATE) {
+    // 유저의 무기별 발사 횟수 검증
+    // 유저가 레드or자동소총 발수 제약없음 / 아니면 핸드건은 2발 제한 나머진 1발
+    if (
+      !(
+        user.characterData.characterType === CHARACTER_TYPE.RED ||
+        user.characterData.weapon === CARD_TYPE.AUTO_RIFLE
+      )
+    ) {
+      // 핸드건인 경우 2발 제한
+      if (user.characterData.weapon === CARD_TYPE.HAND_GUN && user.characterData.bbangCount >= 2) {
+        console.log('발사 실패: 핸드건은 2발로 제한');
 
-      const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
+        // 사용자 자신의 정보만 업데이트
+        const S2CUserUpdateNotification = { user: user.getAllUsersData() };
 
-      const userUpdateNotification = createResponse(
-        HANDLER_IDS.USER_UPDATE_NOTIFICATION,
-        socket.version,
-        socket.sequence,
-        updatePacket,
-      );
+        const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
 
-      socket.write(userUpdateNotification);
+        const userUpdateNotification = createResponse(
+          HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+          socket.version,
+          socket.sequence,
+          updatePacket,
+        );
 
-      return { success: false, failCode: createFailCode(14) };
+        socket.write(userUpdateNotification);
+
+        // userUpdateNotification(room);
+
+        return { success: false, failCode: createFailCode(14) };
+      }
+
+      // 나머지 무기는 1발 제한
+      if (user.characterData.weapon !== CARD_TYPE.HAND_GUN && user.characterData.bbangCount >= 1) {
+        console.log('발사 실패: 이 무기는 1발로 제한');
+
+        // 사용자 자신의 정보만 업데이트
+        const S2CUserUpdateNotification = { user: user.getAllUsersData() };
+
+        const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
+
+        const userUpdateNotification = createResponse(
+          HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+          socket.version,
+          socket.sequence,
+          updatePacket,
+        );
+
+        socket.write(userUpdateNotification);
+
+        // userUpdateNotification(room);
+
+        return { success: false, failCode: createFailCode(14) };
+      }
     }
 
-    // 나머지 무기는 1발 제한
-    if (user.characterData.weapon !== CARD_TYPE.HAND_GUN && user.characterData.bbangCount >= 1) {
-      console.log('발사 실패: 이 무기는 1발로 제한');
+    // 카드를 유저의 핸드에서 제거
+    const useBbangCard = user.characterData.handCards.find((card) => card.type === CARD_TYPE.BBANG);
 
-      // 사용자만 정보 업데이트
-      const S2CUserUpdateNotification = { user: user.getAllUsersData() };
+    if (!useBbangCard) {
+      return { success: false, failCode: createFailCode(11) };
+    }
 
-      const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
+    user.removeHandCard(CARD_TYPE.BBANG);
+    user.characterData.handCardsCount--;
 
-      const userUpdateNotification = createResponse(
-        HANDLER_IDS.USER_UPDATE_NOTIFICATION,
-        socket.version,
-        socket.sequence,
-        updatePacket,
+    // 뱅 발사횟수 1 증가
+    user.increaseBbangCount();
+
+    // 제거한 카드 룸의 덱에 추가
+    room.deckUseCardAdd(CARD_TYPE.BBANG);
+
+    // 개구리군이랑 오토쉴드 적용도 클라에서 처리되는지 확인 필요 -> 안되는거 확인 서버에서 처리할 필요가 있을듯
+    // 상대 캐릭터가 개구리군일 경우
+    if (targetUser.characterData.characterType === CHARACTER_TYPE.FROGGY) {
+      // 자동 쉴드 확률 25% 성공 시 뱅 종료
+      if (Math.floor(Math.random() * 4) === 0) {
+        // 회피 성공
+
+        // 카드 사용 알림
+        const S2CUseCardNotification = {
+          cardType: cardType,
+          userId: user.id,
+          targetUserId: targetUser.id,
+        };
+
+        inGameUsers.forEach((player) => {
+          const gamePacket = { useCardNotification: S2CUseCardNotification };
+
+          const useCardNotification = createResponse(
+            HANDLER_IDS.USE_CARD_NOTIFICATION,
+            player.socket.version,
+            player.socket.sequence,
+            gamePacket,
+          );
+
+          player.socket.write(useCardNotification);
+        });
+
+        userUpdateNotification(room);
+
+        // 종료 response 성공 반환
+        return { success: true, failCode: createFailCode(0) };
+      }
+    }
+
+    // 상대가 오토 쉴드를 가지고 있는 경우
+    if (targetUser.characterData.equips.includes(CARD_TYPE.AUTO_SHIELD)) {
+      // 자동 쉴드 확률 25% 성공 시 뱅 종료
+      if (Math.floor(Math.random() * 4) === 0) {
+        // 회피 성공
+
+        // 카드 사용 알림
+        const S2CUseCardNotification = {
+          cardType: cardType,
+          userId: user.id,
+          targetUserId: targetUser.id,
+        };
+
+        inGameUsers.forEach((player) => {
+          const gamePacket = { useCardNotification: S2CUseCardNotification };
+
+          const useCardNotification = createResponse(
+            HANDLER_IDS.USE_CARD_NOTIFICATION,
+            player.socket.version,
+            player.socket.sequence,
+            gamePacket,
+          );
+
+          player.socket.write(useCardNotification);
+        });
+
+        userUpdateNotification(room);
+
+        // 종료 response 성공 반환
+        return { success: true, failCode: createFailCode(0) };
+      }
+    }
+    // // 쉴드가 1개라도 있으면 대처가능 UI 발생은 클라에서 처리되는 것 확인 완료 // 상어군일경우도 클라에서 처리되는거 확인완료
+
+    // if (targetUser.characterData.handCards.find((card) => card.type === CARD_TYPE.SHIELD)) {
+    // user.setCharacterStateType(CHARACTER_STATE_TYPE.BBANG_SHOOTER);
+    // user.setStateTargetUserId(targetUser.id);
+    //   user.setNextCharacterStateType(CHARACTER_STATE_TYPE.NONE_CHARACTER_STATE);
+    //   user.setNextStateAt(Date.now() + 10000);
+
+    targetUser.setCharacterStateType(CHARACTER_STATE_TYPE.BBANG_TARGET);
+    targetUser.setStateTargetUserId(user.id);
+    // targetUser.setNextCharacterStateType(CHARACTER_STATE_TYPE.NONE_CHARACTER_STATE);
+    //   targetUser.setNextStateAt(Date.now() + 10000);
+    // }
+
+    // targetUser.decreaseHp();
+
+    const S2CUseCardNotification = {
+      cardType: cardType,
+      userId: user.id,
+      targetUserId: targetUser.id,
+    };
+
+    inGameUsers.forEach((player) => {
+      const gamePacket = { useCardNotification: S2CUseCardNotification };
+
+      const useCardNotification = createResponse(
+        HANDLER_IDS.USE_CARD_NOTIFICATION,
+        player.socket.version,
+        player.socket.sequence,
+        gamePacket,
       );
 
-      socket.write(userUpdateNotification);
+      player.socket.write(useCardNotification);
+    });
 
-      return { success: false, failCode: createFailCode(14) };
-    }
-  }
+    userUpdateNotification(room);
 
-  // 카드를 유저의 핸드에서 제거
-  const useBbangCard = user.characterData.handCards.find((card) => card.type === CARD_TYPE.BBANG);
+    return {
+      success: true,
+      failCode: createFailCode(0),
+    };
+  } else {
+    // 타겟이 NONE 상태가 아니라서 공격 진행x
+    // 사용자 자신의 정보만 업데이트
+    const S2CUserUpdateNotification = { user: user.getAllUsersData() };
 
-  if (!useBbangCard) {
-    return { success: false, failCode: createFailCode(11) };
-  }
+    const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
 
-  user.removeHandCard(CARD_TYPE.BBANG);
-  user.characterData.handCardsCount--;
-
-  // 뱅 발사횟수 1 증가
-  user.increaseBbangCount();
-
-  // 제거한 카드 룸의 덱에 추가
-  room.deckUseCardAdd(CARD_TYPE.BBANG);
-
-  // 개구리군이랑 오토쉴드 적용도 클라에서 처리되는지 확인 필요 -> 안되는거 확인 서버에서 처리할 필요가 있을듯
-  
-
-  // // 쉴드가 1개라도 있으면 대처가능 UI 발생은 클라에서 처리되는 것 확인 완료 // 상어군일경우도 클라에서 처리되는거 확인완료
-
-  // if (targetUser.characterData.handCards.find((card) => card.type === CARD_TYPE.SHIELD)) {
-  // user.setCharacterStateType(CHARACTER_STATE_TYPE.BBANG_SHOOTER);
-  // user.setStateTargetUserId(targetUser.id);
-  //   user.setNextCharacterStateType(CHARACTER_STATE_TYPE.NONE_CHARACTER_STATE);
-  //   user.setNextStateAt(Date.now() + 10000);
-
-  targetUser.setCharacterStateType(CHARACTER_STATE_TYPE.BBANG_TARGET);
-  targetUser.setStateTargetUserId(user.id);
-  // targetUser.setNextCharacterStateType(CHARACTER_STATE_TYPE.NONE_CHARACTER_STATE);
-  //   targetUser.setNextStateAt(Date.now() + 10000);
-  // }
-
-  // targetUser.decreaseHp();
-
-  const S2CUseCardNotification = {
-    cardType: cardType,
-    userId: user.id,
-    targetUserId: targetUser.id,
-  };
-
-  inGameUsers.forEach((player) => {
-    const gamePacket = { useCardNotification: S2CUseCardNotification };
-
-    const useCardNotification = createResponse(
-      HANDLER_IDS.USE_CARD_NOTIFICATION,
-      player.socket.version,
-      player.socket.sequence,
-      gamePacket,
+    const userUpdateNotification = createResponse(
+      HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+      socket.version,
+      socket.sequence,
+      updatePacket,
     );
 
-    player.socket.write(useCardNotification);
-  });
+    socket.write(userUpdateNotification);
 
-  userUpdateNotification(room);
+    // userUpdateNotification(room);
 
-  return {
-    success: true,
-    failCode: createFailCode(0),
-  };
+    return {
+      success: false,
+      failCode: createFailCode(10),
+    };
+  }
 };
 
 export default bbang;
--- src/utils/notification/state/bbangTarget.notification.js
+++ src/utils/notification/state/bbangTarget.notification.js
@@ -1,38 +1,71 @@
 import { createResponse } from '../../response/createResponse.js';
 import roomList from '../../../model/room/roomList.class.js';
-import { CHARACTER_STATE_TYPE } from '../../../constants/user.enum.js';
+import { CHARACTER_STATE_TYPE, CHARACTER_TYPE } from '../../../constants/user.enum.js';
 import HANDLER_IDS from '../../../constants/handlerIds.js';
 import userUpdateNotification from '../user/userUpdate.notification.js';
 import playerList from '../../../model/player/playerList.class.js';
 import { CARD_TYPE } from '../../../constants/card.enum.js';
 
-// 피해 받기를 선택하면 실행되는 코드
+// state가 bbangTarget인 상태에서 피해 받기를 선택하면 실행되는 코드
 // 상어군으로 공격하면 쉴드가 1개 이하일때 자동으로 피해받기에 들어가지는 것 확인 완료
 const bbangTargetNotification = async ({ socket, player, reactionType }) => {
   try {
     const room = roomList.getRoom(player.currentRoomId);
     const targetUser = playerList.getPlayer(player.characterData.stateTargetUserId);
 
-    console.log(targetUser);
-
     // 공격자의 무기가 D이글인지 아닌지로 받을 데미지 변동
     // 피해받기 전 체력 기록
     const playerHp = player.characterData.hp;
     if (targetUser.characterData.weapon === CARD_TYPE.DESERT_EAGLE) player.decreaseHp();
     player.decreaseHp();
-    const getDamage = playerHp - player.characterData.hp; // 받은 데미지 -> 말랑 데미지 계산에 사용
-
-    // 피격자가 사망했고, 게임 내에 가면군 유저가 존재한다면 -> 사망한 피격자의 카드들을 가면군에게 전달
-
-    // 피격자가 생존했고, 피격자의 캐릭터가 말랑이라면 -> 받은 데미지 만큼 카드 드로우
-
-    // 피격자가 생존했고, 피격자의 캐릭터가 슬라임이라면 -> 공격자의 핸드 카드를 1장 랜덤으로 훔침
+    if(player.characterData.hp < 0) player.setHp(0);
 
     player.setCharacterStateType(CHARACTER_STATE_TYPE.NONE_CHARACTER_STATE);
     player.setStateTargetUserId(0);
 
-    userUpdateNotification(room);
+    const getDamage = playerHp - player.characterData.hp; // 받은 데미지 -> 말랑 데미지 계산에 사용
 
+    // 피격자가 사망했고, 게임 내에 가면군 유저가 존재한다면 -> 사망한 피격자의 카드들을 가면군에게 전달
+    const maskUser = room.users.find((user) => user.characterData.characterType === CHARACTER_TYPE.MASK);
+
+    if(player.characterData.hp <= 0 && maskUser) {
+      // 사망한 유저의 카드를 mask 캐릭터에게 전달
+
+      for (let i = 0; i < player.characterData.handCards.length; i++) {
+        const cardData = player.characterData.handCards[0];
+        maskUser.characterData.handCards.push(cardData);
+        maskUser.increaseHandCardsCount();
+        player.characterData.handCards.splice(0, 1);
+        player.decreaseHandCardsCount();
+      }
+
+      userUpdateNotification(room);
+      // 위 조건을 만족할 시 아래 과정은 생략
+      return;
+    }
+
+    // 피격자가 생존했고, 피격자의 캐릭터가 말랑이라면 -> 받은 데미지 만큼 카드 드로우
+    if (player.characterData.characterType === CHARACTER_TYPE.MALANG) {
+      for (let i = 0; i < getDamage; i++) {
+        // 덱에서 카드 드로우
+        player.addHandCard();
+        player.increaseHandCardsCount();
+      }
+    }
+
+    // 피격자가 생존했고, 피격자의 캐릭터가 슬라임이라면 -> 공격자의 핸드 카드를 1장 랜덤으로 훔침
+    if (player.characterData.characterType === CHARACTER_TYPE.PINK_SLIME) {
+      // 공격자의 핸드에서 1장 가져옴
+      // 랜덤 인덱스로 1장 
+      const randomIndex = Math.floor(Math.random()*targetUser.characterData.handCards.length);
+      const cardData = targetUser.characterData.handCards[randomIndex];
+      player.characterData.handCards.push(cardData);
+      player.increaseHandCardsCount();
+      targetUser.characterData.handCards.splice(randomIndex, 1);
+      targetUser.decreaseHandCardsCount();
+    }
+
+    userUpdateNotification(room);
   } catch (err) {
     console.error(err);
   }
