b8209f4859182df9a753d46bba29ddcd650db81e
hjy2007 <wjddud2007@gmail.com>
2024-12-03 11:34:47 +0900

    fix deathMatchReaction.notification.js

--- src/utils/notification/card/deathMatchReaction.notification.js
+++ src/utils/notification/card/deathMatchReaction.notification.js
@@ -1,35 +1,46 @@
 import { REACTION_TYPE } from '../../../constants/card.enum.js';
 import HANDLER_IDS from '../../../constants/handlerIds.js';
 import { CHARACTER_STATE_TYPE } from '../../../constants/user.enum.js';
+import playerList from '../../../model/player/playerList.class.js';
 import roomList from '../../../model/room/roomList.class.js';
 import createFailCode from '../../response/createFailCode.js';
+import { createResponse } from '../../response/createResponse.js';
 
 const deathMatchReactionNotification = ({ socket, player, reactionType }) => {
-  // state를 찾아서 계한테 반응이 가게끔 만들어야 함
-  // user
-  const user = player;
-  const room = roomList.getRoom(user.currentRoomId);
+  try {
+    const user = playerList.getPlayer(socket.id);
+    const room = roomList.getRoom(user.currentRoomId);
 
-  user.decreasHp();
+    user.decreaseHp();
+    user.setCharacterStateType(user.characterData.stateInfo.nextState);
+    user.setNextStateAt(0);
 
-  const inGameUsers = Array.from(room.getAllPlayers().values());
+    const targetUser = playerList.getPlayer(user.characterData.stateInfo.stateTargetUserId);
 
-  inGameUsers.forEach((player) => {
-    const S2CUserUpdateNotification = { user: user.getAllUsersData() };
+    targetUser.setCharacterStateType(targetUser.characterData.stateInfo.nextState);
+    targetUser.setNextStateAt(0);
 
-    const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
+    const inGameUsers = Array.from(room.getAllPlayers().values());
 
-    const userUpdateNotification = createResponse(
-      HANDLER_IDS.USER_UPDATE_NOTIFICATION,
-      player.socket.version,
-      player.socket.sequence,
-      updatePacket,
-    );
+    inGameUsers.forEach((player) => {
+      const S2CUserUpdateNotification = { user: user.getAllUsersData() };
 
-    player.socket.write(userUpdateNotification);
-  });
+      const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
 
-  return { succes: true, failcode: createFailCode(0) };
+      const userUpdateNotification = createResponse(
+        HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+        player.socket.version,
+        player.socket.sequence,
+        updatePacket,
+      );
+
+      player.socket.write(userUpdateNotification);
+    });
+
+    return { succes: true, failcode: createFailCode(0) };
+  } catch (error) {
+    console.error('현피 리액션 진행중 에러:', error);
+  }
 };
 
 export default deathMatchReactionNotification;
