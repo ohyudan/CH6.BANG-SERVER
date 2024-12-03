d3c7e548aacf674a9c0d86f5d7d890d7e6804761
hjy2007 <wjddud2007@gmail.com>
2024-12-03 11:32:29 +0900

    fix reactionIndex
    deathMatchReaction 추가

--- src/handlers/inGame/reaction/reactionIndex.js
+++ src/handlers/inGame/reaction/reactionIndex.js
@@ -1,6 +1,7 @@
 import { CHARACTER_STATE_TYPE } from '../../../constants/user.enum.js';
 import bigBnangTargetNotification from '../../../utils/notification/state/bigBbangTarget.notification.js';
 import bigBnangShooterNotification from '../../../utils/notification/state/bigBbangShooter.notification.js';
+import deathMatchReactionNotification from '../../../utils/notification/card/deathMatchReaction.notification.js';
 const reactionAction = {
   [CHARACTER_STATE_TYPE.BIG_BBANG_TARGET]: {
     action: bigBnangTargetNotification,
@@ -8,6 +9,9 @@ const reactionAction = {
   // [CHARACTER_STATE_TYPE.BBANG_SHOOTER]: {
   //   action: bigBnangShooterNotification,
   // },
+  [CHARACTER_STATE_TYPE.DEATH_MATCH_TURN_STATE]: {
+    action: deathMatchReactionNotification,
+  }
 };
 
 export default reactionAction;
