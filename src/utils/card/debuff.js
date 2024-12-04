1114aa43b7c4872ffad942ee7c5608fe859cc396
hjy2007 <wjddud2007@gmail.com>
2024-12-04 09:31:57 +0900

    add debuff.js

---
+++ src/utils/card/debuff.js
@@ -0,0 +1,55 @@
+import { CARD_TYPE } from '../../constants/card.enum.js';
+import HANDLER_IDS from '../../constants/handlerIds.js';
+import playerList from '../../model/player/playerList.class.js';
+import roomList from '../../model/room/roomList.class.js';
+import createFailCode from '../response/createFailCode.js';
+import { createResponse } from '../response/createResponse.js';
+
+const debuff = ({ socket, cardType, targetUserId }) => {
+  const user = playerList.getPlayer(socket.id);
+  const room = roomList.getRoom(user.currentRoomId);
+  const targetUser = playerList.getPlayer(targetUserId.low)
+
+  targetUser.addDebuff(cardType)
+  
+  user.removeHandCard(cardType);
+  user.characterData.handCardsCount--;
+ 
+  const inGameUsers = Array.from(room.getAllPlayers().values());
+
+  const S2CUseCardNotification = {
+    cardType: cardType,
+    userId: user.id,
+    targetUserId: targetUser.id,
+  };
+
+  inGameUsers.forEach((player) => {
+    const gamePacket = { useCardNotification: S2CUseCardNotification };
+
+    const useCardNotification = createResponse(
+      HANDLER_IDS.USE_CARD_NOTIFICATION,
+      player.socket.version,
+      player.socket.sequence,
+      gamePacket,
+    );
+
+    player.socket.write(useCardNotification);
+
+    const S2CUserUpdateNotification = { user: player.getAllUsersData() };
+
+    const updatePacket = { userUpdateNotification: S2CUserUpdateNotification };
+
+    const userUpdateNotification = createResponse(
+      HANDLER_IDS.USER_UPDATE_NOTIFICATION,
+      player.socket.version,
+      player.socket.sequence,
+      updatePacket,
+    );
+
+    player.socket.write(userUpdateNotification);
+  });
+
+  return { success: true, failCode: createFailCode(0) };
+};
+
+export default debuff;
